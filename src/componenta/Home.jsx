b98c0c40f9523947f495e82035dc01b5774924cf
Asalef <asalef10@gmail.com>
2021-05-25 15:52:20 +0300

    changeNameDog

--- src/componenta/Home.jsx
+++ src/componenta/Home.jsx
@@ -4,9 +4,9 @@ export default function DogCardDetils() {
     return (
         <div>
             <DogCard nameDog="POKA" typeDog="Pitbull" Age="18" />
-            <DogCard nameDog="POKA" typeDog="Pitbull" Age="14" />
-            <DogCard nameDog="POKA" typeDog="Pitbull" Age="4" />
-            <DogCard nameDog="POKA" typeDog="Pitbull" Age="4" />
+            <DogCard nameDog="Reksi" typeDog="AskiSeberi" Age="14" />
+            <DogCard nameDog="Tarzan" typeDog="pincher" Age="4" />
+            <DogCard nameDog="Loni" typeDog="Bultiler" Age="4" />
 
         </div>
     )
