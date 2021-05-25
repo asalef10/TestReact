df3fdf6213690d5d80b7587ea85725166b029b11
Asalef <asalef10@gmail.com>
2021-05-25 16:28:06 +0300

    upp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import DogCardDetils from './componenta/Home.jsx'
-// import DogCard from './componenta/cardDog.jsx';
 
 
 
--- src/componenta/Home.jsx
+++ src/componenta/Home.jsx
@@ -1,8 +1,22 @@
 import DogCard from './cardDog.jsx'
 
 export default function DogCardDetils() {
+
+    // נסיון ליצור את האובייקטים במערך 
+
+    // let ArryObject = [{
+    //     nameDog: "POKA",
+    //     typeDog: "Pitbull",
+    //     Age="18",
+
+    // },
+    // ]
+
     return (
         <div>
+
+            {/* <DogCard nameDog={ArryObject.nameDog} typeDog={ArryObject.typeDog} Age={ArryObject.Age} /> */}
+                                    
             <DogCard nameDog="POKA" typeDog="Pitbull" Age="18" />
             <DogCard nameDog="Reksi" typeDog="AskiSeberi" Age="14" />
             <DogCard nameDog="Tarzan" typeDog="pincher" Age="4" />
